4ed15d2a6223bf5fa5616ff6c51f86b4e01fadb2
Mustafa Omar <m.omar@insta-lon-m2s-MacBook-Pro.local>
2024-02-23 17:16:44 +0000

    updaing vim

--- setup.sh
+++ setup.sh
@@ -1,30 +1,37 @@
 # clear
+mkdir ~/.config
+rm -r ~/.config/nvim
 sudo rm -r  ~/.vimrc ~/.vim
-mkdir  ~/.vim
+brew uninstall vim
+mkdir ./temp
+cd temp
 # vimrc
-cp vimrc ~/.vimrc
+curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz
+tar xzf nvim-macos.tar.gz
+mkdir ~/bin
+mv ./nvim-macos ~/bin/nvim-macos
+echo "alias vim='~/bin/nvim-macos/bin/nvim'" >> ~/.zshrc
+source ~/.zshrc -l
+cd ../
+
+# move config to .config
+cp -r nvim_config ~/.config/nvim
+
+
+# package manager
+sudo rm -r ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+brew install ripgrep
+
+# clean up
+rm -r temp
 # Vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-# ale
-cd ~/.vim/bundle
-git clone https://github.com/dense-analysis/ale.git
-# flake8
-pip install flake8
-mkdir -p ~/.vim/pack/flake8/start/
-cd ~/.vim/pack/flake8/start/
-git clone https://github.com/nvie/vim-flake8.git
-# colors
-mkdir ~/.vim/colors/
-cd ~/.vim/colors
-git clone https://github.com/gosukiwi/vim-atom-dark.git
-cp vim-atom-dark/colors/* .
-sudo rm -r vim-atom-dark
 #
-vim +PluginInstall +qa
-cd ~/
+
 # Powerlevel10k zhs theme, unrelated to vim !!!
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-cp .p10k.zsh ~/.p10k.zsh
-cp .zshrc ~/.zshrc
-exec zsh
+#git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+#echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+#cp .p10k.zsh ~/.p10k.zsh
+#cp .zshrc ~/.zshrc
+#exec zsh
