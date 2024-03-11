dad3c71c48d457cae1edc0e5902598025ed23295
Mustafa Omar <mustafa12211@hotmail.co.uk>
2024-03-11 13:24:21 +0000

    minor changes

--- .wezterm.lua
+++ .wezterm.lua
@@ -19,7 +19,6 @@ config.window_frame = {
   border_bottom_color = 'silver',
 }
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
--- This is where you actually apply your config choices
--- and finally, return the configuration to wezterm
+
 return config
 
--- nvim_config/lua/default/remap.lua
+++ nvim_config/lua/default/remap.lua
@@ -1,6 +1,8 @@
 vim.keymap.set('n', '<leader><leader>', 'za', {noremap = true, silent = true})
 
 vim.keymap.set("n", "<leader>pv", ":NvimTreeFindFile!<Enter>", {noremap = true, silent = true})
+vim.keymap.set("n", "<leader>qn", ":cn<Enter>", {noremap = true, silent = true})
+vim.keymap.set("n", "<leader>qp", ":cp<Enter>", {noremap = true, silent = true})
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
